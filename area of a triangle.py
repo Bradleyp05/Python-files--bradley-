e97cf544b969745519ea455aa2a5055984dd894b
Bradley Proctor <114503436+Bradleyp05@users.noreply.github.com>
2023-09-18 10:21:35 +0100

    upload area of a triangle

---
+++ area of a triangle.py
@@ -0,0 +1,14 @@
+def triangle_area():
+    base = int(input("Enter the base of the triangle"))
+    height = int(input("Enter the height of the triangle"))
+    area = base*height/2
+    print("The area of your triangle is" , area)
+
+triangle_area()
+
+def power_calculator():
+    voltage = int(input("Please enter the voltage"))
+    current = int(input("Please enter the current"))
+    power = current
+
+
